24fdbe25c3d7be3496f2fb66e9d1054f0e2eebc5
Tom <tom.kemper@hu.nl>
2022-05-18 11:36:22 +0200

    Hello World Jersey-style

---
+++ src/main/java/nl/hu/bep/azuredemo/JerseyConfig.java
@@ -0,0 +1,12 @@
+package nl.hu.bep.azuredemo;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/restservices")
+public class JerseyConfig extends ResourceConfig {
+    public JerseyConfig(){
+        packages("nl.hu.bep.azuredemo.rest");
+    }
+}
