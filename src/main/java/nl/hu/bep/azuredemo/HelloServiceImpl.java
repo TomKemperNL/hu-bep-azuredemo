bc763e1d7eb73726113ac38129cbacce18f81203
Tom <tom.kemper@hu.nl>
2022-05-25 12:04:37 +0200

    DI voorbeeld in Jersey

--- src/main/java/nl/hu/bep/azuredemo/HelloServiceImpl.java
+++ src/main/java/nl/hu/bep/azuredemo/HelloServiceImpl.java
@@ -1,6 +1,12 @@
 package nl.hu.bep.azuredemo;
 
 public class HelloServiceImpl implements HelloService {
+
+    public HelloServiceImpl(){
+        System.out.println("Ik wordt geconstruct!");
+    }
+
+
     public String getMessage(){
         return "Hello World";
     }
--- src/main/java/nl/hu/bep/azuredemo/ServiceBinder.java
+++ src/main/java/nl/hu/bep/azuredemo/ServiceBinder.java
@@ -2,10 +2,17 @@ package nl.hu.bep.azuredemo;
 
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 
+import javax.inject.Singleton;
+
 public class ServiceBinder extends AbstractBinder {
     @Override
     protected void configure() {
+        //Dit maakt 1 HelloServiceImpl instantie die hergebruikt wordt:
+        //bind(HelloServiceImpl.class).to(HelloService.class).in(Singleton.class);
+
+        //Dit maakt elk request een nieuwe HelloServiceImpl instantie:
         bind(HelloServiceImpl.class).to(HelloService.class);
 
+        //De één is niet noodzakelijk beter dan de ander, maar per situatie kan de één handiger zijn dan de andere.
     }
 }
